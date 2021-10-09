7a7d90a5823f2d3c8b53a6efb504148d688eaa51
Ravi Mohan <ravi.mohan@roonga.com.au>
2021-10-10 10:11:46 +1100

    basic interface

--- 06-Interface-and-Class/dotnet/Program.cs
+++ 06-Interface-and-Class/dotnet/Program.cs
@@ -2,6 +2,20 @@
 
 namespace dotnet
 {
+    public interface IAnimal
+    {
+        string Speak();
+
+    }
+
+    public class Dog : IAnimal
+    {
+        public string Speak()
+        {
+            return "bark";
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
--- 06-Interface-and-Class/golang/main.go
+++ 06-Interface-and-Class/golang/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
+
+type Animal interface {
+	getName() string
+	speak() string
+}
+
+// Dog
+type Dog struct {
+	Name string
+}
+
+func (d Dog) getName() string {
+	return d.Name
+}
+
+func (d Dog) speak() string {
+	return "bark"
+}
+
+// Cat
+type Cat struct {
+	Name string
+}
+
+func (c Cat) getName() string {
+	return c.Name
+}
+
+func (c Cat) speak() string {
+	return "meow"
+}
+
+func getSound(animal Animal) {
+	fmt.Println(animal.speak())
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	var rover = Dog{Name: "rover"}
+	var tubby = Cat{Name: "tubby"}
+
+	var animals = [2]Animal{rover, tubby}
+
+	for _, a := range animals {
+		fmt.Println(a.getName(), " the ", reflect.TypeOf(a), " says ", a.speak())
+	}
 }
