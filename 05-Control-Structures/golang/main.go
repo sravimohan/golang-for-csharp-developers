d14f426a6fb2c97dcba542a3009971d063456add
Ravi Mohan <ravi.mohan@roonga.com.au>
2021-10-10 08:56:41 +1100

    update

--- 05-Control-Structures/golang/main.go
+++ 05-Control-Structures/golang/main.go
@@ -19,17 +19,18 @@ func main() {
 	}
 
 	// switch
-	// - break not required, has different meaning
-	// - contine can be added
+	// - break is implicit in the case block
+	// - break statement will break out of switch
 	// - fallthrough
 	id := 0
 	fmt.Print("switch")
 	switch id {
 	case 0:
 		fmt.Print(" : ", 0)
+		fallthrough
 	case 1:
 		fmt.Print(" : ", 1)
-	case 2, 3:
+	case 2, 3: // multiple case in a single block
 		fmt.Print(" : ", 2, 3)
 	default:
 		fmt.Print(" : ", "invalid")
@@ -41,7 +42,7 @@ func main() {
 		fmt.Print(" : ", i)
 	}
 
-	// nested for
+	// nested for with lable
 	// - break
 	// - labeled break - used for nested loops
 	fmt.Print("\nnested for")
