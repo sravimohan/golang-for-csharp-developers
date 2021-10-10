7538e2ba71ad71b69d2ec532c2ef021556170174
Ravi Mohan <ravi.mohan@roonga.com.au>
2021-10-11 07:38:01 +1100

    final

--- 06-Interface-and-Class/golang/main.go
+++ 06-Interface-and-Class/golang/main.go
@@ -14,30 +14,24 @@ type Description struct {
 	Name string
 }
 
-func (a Description) GetName() string {
-	return a.Name
+func (d *Description) GetName() string {
+	return d.Name
 }
 
-// Dog
 type Dog struct {
 	Description
 }
 
-func (a *Dog) Speak() string {
+func (d *Dog) Speak() string {
 	return "bark"
 }
 
-// Cat
 type Cat struct {
 	Description
 }
 
 func (c *Cat) Speak() string {
 	return "meow"
-}
-
-func getSound(animal Animal) {
-	fmt.Println(animal.Speak())
 }
 
 func main() {
@@ -47,7 +41,8 @@ func main() {
 	tubby := Cat{}
 	tubby.Name = "tubby"
 
-	var animals = [2]Animal{&rover, &tubby}
+	animals := [2]Animal{&rover, &tubby}
+
 	for _, a := range animals {
 		fmt.Printf("%s the %s says %s\n", a.GetName(), reflect.TypeOf(a), a.Speak())
 	}
