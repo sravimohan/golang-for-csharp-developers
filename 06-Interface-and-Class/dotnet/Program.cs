bb16a054ddb5bde218a69bd683cc9ca4e99dcc9e
Ravi Mohan <ravi.mohan@roonga.com.au>
2021-10-11 07:16:12 +1100

    dotnet impl

--- 06-Interface-and-Class/dotnet/Program.cs
+++ 06-Interface-and-Class/dotnet/Program.cs
@@ -4,15 +4,37 @@ namespace dotnet
 {
     public interface IAnimal
     {
+        string Name { get; }
         string Speak();
 
     }
 
     public class Dog : IAnimal
     {
+        public string Name { get; }
+
+        public Dog(string name)
+        {
+            Name = name;
+        }
+
         public string Speak()
         {
             return "bark";
+        }
+    }
+
+    public class Cat : IAnimal
+    {
+        public string Name { get; }
+        public Cat(string name)
+        {
+            Name = name;
+        }
+
+        public string Speak()
+        {
+            return "meow";
         }
     }
 
@@ -20,7 +42,15 @@ namespace dotnet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rover = new Dog("rover");
+            var tubby = new Cat("tubby");
+
+            var animals = new IAnimal[] { rover, tubby };
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine($"{animal.Name} the {animal.GetType()} says {animal.Speak()}");
+            }
         }
     }
 }
